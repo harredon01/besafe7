8e6c6debc86ed0caf5b763577a203ab095bbaa21
Hoovert <harredon01@gmail.com>
2020-07-20 17:42:11 -0500

    Updating dependencies

--- config/cors.php
+++ config/cors.php
@@ -11,6 +11,21 @@ return [
      | to accept any value.
      |
      */
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
