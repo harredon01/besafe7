5ff38ba6db564513d32f457ee197ab4e8ac0fbd7
Hoovert <harredon01@gmail.com>
2020-11-08 20:23:01 -0500

    Fixing conflicts

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,22 +1,31 @@
 @extends(config("app.views").'layouts.app')
 
 @section('content')
-<div class="container" ng-controller="MapLocationCtrl">
+<div class="container" ng-controller="MapCtrl">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">               
-                
-                <div class="panel-heading"></div>
-                      
+                @if (isset($following))
 
+
+                <div class="panel-heading">Siguiendo a {{ $following->firstName }} {{ $following->lastName }}</div>
+
+                <div id="map" data-tap-disabled="true"></div>
                 <div class="panel-body">
-                    <button ng-click="saveLocation()" class="btn btn-primary">Guardar</button>
-                    <button ng-click="cancel()" class="btn btn-primary">Cancelar</button>
-                    <div id="map" data-tap-disabled="true"></div>
+
+                </div>
+                @else 
+                <div class="panel-heading">No hay nadie compartiendo con ese código
+
+
+                    <div class="panel-body">
+
+                    </div>
+
+                    @endif
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-@endsection
+    @endsection
