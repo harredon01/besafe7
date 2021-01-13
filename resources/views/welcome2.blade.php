a67b1b8d6ac83405add86f1ed1f9c2416b9d55f6
Hoovert <harredon01@gmail.com>
2021-01-12 21:37:38 -0500

    Set priority and expiration on sitemap

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -8,8 +8,8 @@
         <loc>{{url("/a/reports/".$category['url'])}}</loc>
         @endif
         <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($category['updated_at'])) }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
     </url>
     @endforeach
     @foreach ($merchants as $merchant)
@@ -17,14 +17,14 @@
         <loc>{{url("/a/merchant/".$merchant->slug)}}</loc>
         <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($merchant->updated_at)) }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <priority>0.7</priority>
     </url>
     @foreach ($merchant->cats as $cat)
     <url>
         <loc>{{url("/a/products/".$cat->url."?merchant_id=".$merchant->id)}}</loc>
         <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($merchant->updated_at)) }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <priority>0.8</priority>
     </url>
     @endforeach
     @endforeach
@@ -39,43 +39,43 @@
     <url>
         <loc>{{url("/a/contact-us/vets")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/contact-us/shop")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/contact-us/lost")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/contact-us/sale")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/contact-us/bla")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/faq")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
     <url>
         <loc>{{url("/a/terms")}}</loc>
         <lastmod>2020-12-11T21:40:41Z</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.6</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.3</priority>
     </url>
 </urlset>
