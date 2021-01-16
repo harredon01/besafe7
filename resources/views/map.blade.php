a6cf88818df67f266b30a36da8287d16d11ce122
Hoovert <harredon01@gmail.com>
2021-01-16 02:46:40 -0500

    First commit

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">               
                 @if (isset($following))
-
+ 
 
                 <div class="panel-heading">Siguiendo a {{ $following->firstName }} {{ $following->lastName }}</div>
 
