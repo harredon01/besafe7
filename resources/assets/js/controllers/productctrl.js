6f2b3e10d51a0c4d2ac37a2c1f17bd2d53bfe6d2
Hoovert <harredon01@gmail.com>
2021-01-16 02:12:24 -0500

    Finalll

--- resources/assets/js/controllers/productctrl.js
+++ resources/assets/js/controllers/productctrl.js
@@ -421,16 +421,6 @@
                     $scope.updateProductsCart(args, false);
                 });
                 $scope.clean = function () {
-                    angular.forEach(angular.element(".product-attributes"), function (value, key) {
-                        var a = angular.element(value);
-                        var obj = JSON.parse(a.html());
-                        var html = "";
-                        for (x in obj) {
-                            html += x + ": " + obj[x] + " <br/>";
-                        }
-                        a.html(html)
-                        a.css("display", "block");
-                    });
                 }
                 $scope.selectVariant = function (product) {
                     for (let item in product.variants) {
