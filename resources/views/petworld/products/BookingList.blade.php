c56e4a2f23ab09a5291f35adcacd4caf577f8a9d
Hoovert <harredon01@gmail.com>
2021-05-16 13:34:01 -0500

    Fixing presentation details

--- resources/views/petworld/products/BookingList.blade.php
+++ resources/views/petworld/products/BookingList.blade.php
@@ -16,9 +16,9 @@
             @{{ item.attributes.name}} @{{ item.attributes.from | date:'medium'}}
         </td>
         <td class="col-md-2" ng-show="item.pending">                              
-            <a href="javascript:;" class="text-orange" ng-click="programItem(item)">Programar</a>
+            <a href="javascript:;" class="text-orange" ng-click="programItem(item)" style="border: 2px solid #ff7c00;border-radius: 5px;padding: 0 10px;">Programar</a>
         </td>
-        <td class="col-md-2 text-black" ng-hide="item.pending">                              
+        <td class="col-md-2 text-black" ng-hide="item.pending">
             
         </td>
     </tr>
